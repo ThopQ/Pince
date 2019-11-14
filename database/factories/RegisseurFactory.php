fab0b055cb019c7681cdbcff9c9eda1473b85081
ThopQ <ti@netivity.ch>
2019-11-15 00:17:55 +0100

    make:model Regisseur -a

---
+++ database/factories/RegisseurFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Regisseur;
+use Faker\Generator as Faker;
+
+$factory->define(Regisseur::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
