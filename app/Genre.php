765c41c63850b74ba4a8e2ccec752013c1b360b7
ThopQ <ti@netivity.ch>
2019-11-15 16:05:49 +0100

    genres now have a name and a description column

--- app/Genre.php
+++ app/Genre.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Genre extends Model
 {
+    //Fillable attributes stored in array
+    protected $fillable = ['name', 'description'];
+
     public function movies()
     {
         return $this->belongsToMany('App\Movie');
--- database/migrations/2019_11_14_231831_create_genres_table.php
+++ database/migrations/2019_11_14_231831_create_genres_table.php
@@ -14,6 +14,8 @@ class CreateGenresTable extends Migration
     public function up()
     {
         Schema::create('genres', function (Blueprint $table) {
+            $table->string('name');
+            $table->string('description');
             $table->bigIncrements('id');
             $table->timestamps();
         });
