0616730c0ba45ff3fdffc2ed47d1a79dc872d129
ThopQ <ti@netivity.ch>
2019-11-18 21:55:35 +0100

    id after content

--- database/migrations/2019_11_14_231831_create_genres_table.php
+++ database/migrations/2019_11_14_231831_create_genres_table.php
@@ -14,9 +14,9 @@ class CreateGenresTable extends Migration
     public function up()
     {
         Schema::create('genres', function (Blueprint $table) {
+            $table->bigIncrements('id');
             $table->string('name');
             $table->string('description');
-            $table->bigIncrements('id');
             $table->timestamps();
         });
     }
